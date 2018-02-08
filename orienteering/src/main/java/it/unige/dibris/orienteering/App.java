0c6c9370e1a47e2c9543a1ddfde09305734bb9e9
angeloferrando <angelo.ferrando42@gmail.com>
2018-02-08 10:11:49 +0100

    added slf4j dependency in pom file

--- orienteering/src/main/java/it/unige/dibris/orienteering/App.java
+++ orienteering/src/main/java/it/unige/dibris/orienteering/App.java
@@ -1,13 +1,14 @@
 package it.unige.dibris.orienteering;
 
+import static spark.Spark.*;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+	public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello World");
     }
 }
